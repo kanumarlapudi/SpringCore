db3c937b74feaa070b7ff08d74647d942d92a571
kanumarlapudi <180020034ce@gmail.com>
2023-04-20 21:54:35 +0530

    first

---
+++ SetterInjection-01/src/in/ineuron/comp/WishMessageGenerator.java
@@ -0,0 +1,50 @@
+package in.ineuron.comp;
+
+import java.util.Date;
+
+//Target class(userDefined=>WishMessageGenerator)
+public class WishMessageGenerator {
+
+	// Dependant class(predefined => java.util.Date)
+	private Date date;
+	
+	static {
+		System.out.println("WishMessageGenerator.class file is loading");
+	}
+	
+	public WishMessageGenerator() {
+		System.out.println("WishMessageGenerator object is instantiated");
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+		System.out.println("Setter method is called to perform setter injection");
+	}
+	
+	// Business Logic method => using dependent object in B.L
+	
+	public String generateBusiness(String userName) {
+		int hour = date.getHours();  //get hour in 24 hour format
+		
+		if(hour<12) {
+			return "Good Morning :: " + userName;
+		}
+		
+		else if(hour<16) {
+			return "Good Afternoon :: " + userName;
+		}
+		
+		else if(hour<20) {
+			return "Good Evening :: " + userName;
+		}
+		
+		else {
+			return "Good Night :: " + userName;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "WishMessageGenerator [date=" + date + "]";
+	}
+}
---
+++ SetterInjection-01/src/in/ineuron/test/test.java
@@ -0,0 +1,25 @@
+package in.ineuron.test;
+
+import in.ineuron.comp.WishMessageGenerator;
+
+public class test {
+
+	public static void main(String[] args) {
+		WishMessageGenerator generator = new WishMessageGenerator();
+		System.out.println("Target Object Before Setter Injection is :: " + generator);
+		
+		java.util.Date date = new java.util.Date();
+		System.out.println("Dependant Object is :: " + date);
+		
+		generator.setDate(date);
+		
+		System.out.println("Target Object after setter Injection :: " + generator);
+		
+		System.out.println();
+		
+		String result = generator.generateBusiness("Harish");
+		System.out.println(result);
+
+	}
+
+}
